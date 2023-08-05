b7d16b875fb7a252d86cf03ffa06437de3ffdaca
Mahnoor Shakeel <139671971+MahnoorShakel@users.noreply.github.com>
2023-08-05 06:20:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var a = 10; // declare and assign a numeric value to variable a
+var b = 5; // declare and assign a numeric value to variable b
+var c = a + b; // declare and assign the result of adding a and b to variable c
+console.log(c); // print the value of c to the console
---
+++ index.ts
@@ -0,0 +1,4 @@
+let a = 10; // declare and assign a numeric value to variable a
+let b = 5; // declare and assign a numeric value to variable b
+let c = a + b; // declare and assign the result of adding a and b to variable c
+console.log(c); // print the value of c to the console
